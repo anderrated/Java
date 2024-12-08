cd5f7958b56d187ee9301ea9361222fcbee555b1
Andrea Laserna <104805730+anderrated@users.noreply.github.com>
2024-12-08 19:03:02 +0800

    prime

---
+++ Prime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = input.nextInt();
+        boolean isPrime = isPrime(num);
+        if(isPrime){
+            System.out.println("The number is a prime number");
+        } else {
+            System.out.println("The number is not a prime number");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if(num <= 1){
+            return false;
+        }
+        for(int i = 2; i < num; i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
